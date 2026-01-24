e58baa3b280df1b397aad46876c85bb6b1bf9258
Tatiana Villa <tatiana@villaema.es>
2026-01-24 19:44:17 +0100

    Peticion de datos a https

--- js/login.js
+++ js/login.js
@@ -13,7 +13,7 @@ async function login() {
     }
 
     try {
-        const response = await fetch("http://localhost:8080/auth/login", {
+        const response = await fetch("https://recursos-catolicos.es:8080/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password })
